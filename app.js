cf027bd1b560383cffe66aabaddb61da44678e95
valentijnkap <info@valentijnkap.nl>
2018-04-09 03:33:20 +0200

    Adding mongo support and registers users

---
+++ app.js
@@ -0,0 +1,86 @@
+'use strict'
+
+var express = require('express')
+var mongo = require('mongodb')
+var bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+var db = null
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+
+mongo.MongoClient.connect(url, function (err, client) {
+	if (err) throw err
+	db = client.db(process.env.DB_NAME)
+})
+
+var port = 8000
+
+express()
+	.set('view engine', 'ejs')
+	.set('views', 'view')
+	.use(express.static('static'))
+	.use(bodyParser.urlencoded({ extended: false }))
+	.get('/', register)
+	.post('/', registerUser)
+	.get('/login', login)
+	.get('/:username', profile)
+	.listen(port)
+
+console.log('The app is working on port: ' + port)
+
+function register (req, res) {
+	var meta = {
+		title: 'Register',
+		caption: 'Photo by Katerina Radvanska -  Unsplash'
+	}
+
+	res.render('pages/register.ejs', meta)
+}
+
+function registerUser (req, res) {
+	db.collection('users').insertOne({
+		username: req.body.username,
+		name: req.body.name,
+		place: req.body.place,
+		email: req.body.email,
+		password: req.body.password
+	}, done)
+
+	function done (err, data) {
+		if (err) throw err
+
+		res.redirect('/' + req.body.username)
+	}
+}
+
+function login (req, res) {
+	var meta = {
+		title: 'Login',
+		caption: 'Photo by Katerina Radvanska -  Unsplash'
+	}
+
+	res.render('pages/login.ejs', meta)
+}
+
+function profile (req, res) {
+	var user = req.params.username
+
+	console.log(user)
+
+	db.collection('users').findOne({
+		username: user
+	}, done)
+
+	var meta = {
+		title: user
+	}
+
+	function done (err, data) {
+		if (err) {
+			throw err
+		} else {
+			res.render('pages/profile.ejs', meta, {data: data})
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Assessment 2
+
+[Banner][banner]
+
+# Places
+
+## Table of contents
+
+* [The story..](#The-story)
+* The making of..
+* Usage
+* To do's
+* Recourses
+
+## (#The-story)
+
+## The making of
+
+## Usage
+
+## To do's
+* [ ] 
+
+## License
+[GPL][license] © [Valentijn Kap](https://github.com/valentijnkap)
+
+[banner]: banner.png
+[license]: https://opensource.org/licenses/GPL-3.0
