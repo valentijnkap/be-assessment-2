330967b2677940e82a0018e2603f8ce924485a73
valentijnkap <info@valentijnkap.nl>
2018-04-25 15:44:22 +0200

    Supporting PUT request and updating users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb')
 var bodyParser = require('body-parser')
 var argon = require('argon2')
 var session = require('express-session')
+var methodOverride = require('method-override')
 
 require('dotenv').config()
 
@@ -22,11 +23,20 @@ express()
   .set('view engine', 'ejs')
   .set('views', 'view')
   .use(express.static('static'))
+  .use(methodOverride('X-HTTP-Method-Override'))
+  .use(methodOverride('_method'))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(session({
     resave: false,
     saveUninitialized: true,
     secret: process.env.SESSION_SECRET
+  }))
+  .use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      var method = req.body._method
+      delete req.body._method
+      return method
+    }
   }))
   .get('/', register)
   .post('/', registerUser)
@@ -34,15 +44,18 @@ express()
   .post('/login', loginUser)
   .get('/logout', logout)
   .get('/feed', feed)
+  .get('/edit', editProfile)
+  .put('/edit', updateUser)
   .get('/:username', profile)
-  .listen(port, function() {
+  .listen(port, function () {
     console.log('The app is working on port: ' + port)
   })
 
 function register (req, res) {
   var meta = {
     title: 'Register',
-    caption: 'Photo by Katerina Radvanska - Unsplash'
+    caption: 'Photo by Katerina Radvanska - Unsplash',
+    message: ''
   }
 
   res.render('pages/register.ejs', meta)
@@ -52,7 +65,22 @@ function registerUser (req, res) {
   var username = req.body.username
   var password = req.body.password
 
-  argon.hash(password).then(insert)
+  db.collection('users').findOne({
+    username: username
+  }, check)
+
+  function check (err, data) {
+    if (!data) {
+      argon.hash(password).then(insert)
+    } else if (data) {
+      var meta = {
+        title: 'Register',
+        caption: 'Photo by Katerina Radvanska - Unsplash',
+        message: 'Sorry, username has already been taken!'
+      }
+      res.render('pages/register.ejs', meta)
+    }
+  }
 
   function insert(hash) {
     db.collection('users').insertOne({
@@ -80,7 +108,11 @@ function login (req, res) {
     caption: 'Photo by Katerina Radvanska - Unsplash',
     message: ''
   }
-  res.render('pages/login.ejs', meta)
+  if (req.session.user == undefined) {
+    res.render('pages/login.ejs', meta)
+  } else if (req.session.user) {
+    res.redirect('/' + req.session.user.username)
+  }
 }
 
 function loginUser (req, res) {
@@ -108,7 +140,7 @@ function loginUser (req, res) {
         req.session.user = {username: data.username}
         res.redirect('/' + data.username)
       } else {
-          var meta = {
+        var meta = {
           title: 'login',
           caption: 'Photo by Katerina Radvanska - Unsplash',
           message: 'Sorry, the password is incorrect!'
@@ -123,8 +155,34 @@ function feed (req, res) {
   if (req.session.user) {
     res.send('Hoi hij doet het')
   } else {
-    res.redirect('/login')
+    loginFirst(res)
   }
+}
+
+function editProfile (req, res) {
+  if (req.session.user) {
+    var meta = {
+      title: 'Edit ' + req.session.user.username
+    }
+
+    db.collection('users').findOne({
+      username: req.session.user.username
+    }, done)
+
+    function done (err, data) {
+      if (err) throw err
+      res.render('pages/edit_profile.ejs', Object.assign({}, {data: data}, meta))
+    }
+  } else {
+    loginFirst(res)
+  }
+}
+
+function updateUser (req, res) {
+  console.log('De put request komt door.')
+  console.log(req.body)
+
+  res.redirect('/' + req.session.user.username)
 }
 
 function logout (req, res) {
@@ -144,14 +202,15 @@ function profile (req, res) {
       username: user
     }, done)
   } else {
-    res.redirect('/login')
-  }
-
-  var meta = {
-    title: user
+    loginFirst(res)
   }
 
   function done (err, data) {
+    var meta = {
+      title: user,
+      session: req.session.user.username
+    }
+
     if (err) {
       throw err
     } else if(!data) {
@@ -160,4 +219,13 @@ function profile (req, res) {
       res.render('pages/profile.ejs', Object.assign({}, {data: data}, meta))
     }
   }
+}
+
+function loginFirst (res) {
+  var meta = {
+      title: 'login',
+      caption: 'Photo by Katerina Radvanska - Unsplash',
+      message: 'You need to login first to make use of our service. Sorry! It`s a privacy thing!'
+    }
+    res.render('pages/login.ejs', meta)
 }
---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+/* Code from: https://stackoverflow.com/questions/13780309/get-filename-from-input-type-file-using-jquery?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa */
+
+$(document).ready(function (){
+	$('.fileInputLabel').click(function() {
+	  $('#fileInput').change(function() {
+	    var filename = $('#fileInput').val()
+	    filename = filename.slice(12)
+	    $('.filename').html(filename)
+	  })
+	})
+})
