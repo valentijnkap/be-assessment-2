5ee008f205c46037d9d0b7cc7805adf60198386e
valentijnkap <info@valentijnkap.nl>
2018-05-10 22:03:13 +0200

    Added a feed and write page. And modified the profile page.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser')
 var argon = require('argon2')
 var session = require('express-session')
 var methodOverride = require('method-override')
+var multer = require('multer')
+var path = require('path')
+var fs = require('fs')
 
 require('dotenv').config()
 
@@ -17,6 +20,23 @@ mongo.MongoClient.connect(url, function (err, client) {
   db = client.db(process.env.DB_NAME)
 })
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var uploadPath = 'static/uploads/' + req.session.user.username
+    if (fs.exists(uploadPath)) {
+      cb(null, uploadPath)
+    } else {
+      fs.mkdir(uploadPath, function () {
+        cb(null, uploadPath)
+      })
+    }
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+var upload = multer({storage: storage})
 var port = 8000
 
 express()
@@ -31,13 +51,6 @@ express()
     saveUninitialized: true,
     secret: process.env.SESSION_SECRET
   }))
-  .use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      var method = req.body._method
-      delete req.body._method
-      return method
-    }
-  }))
   .get('/', register)
   .post('/', registerUser)
   .get('/login', login)
@@ -45,20 +58,27 @@ express()
   .get('/logout', logout)
   .get('/feed', feed)
   .get('/edit', editProfile)
-  .put('/edit', updateUser)
-  .get('/:username', profile)
+  .put('/edit', upload.single('thumb'), updateUser)
+  .get('/write', writeForm)
+  .post('/write', upload.single('banner'), postStory)
+  .get('/users/:username', profile)
+  .get('/stories/:id', storyDetail)
+  .use(notFound)
   .listen(port, function () {
     console.log('The app is working on port: ' + port)
   })
 
 function register (req, res) {
-  var meta = {
-    title: 'Register',
-    caption: 'Photo by Katerina Radvanska - Unsplash',
-    message: ''
+  if (req.session.user) {
+    res.redirect('/feed')
+  } else {
+    var meta = {
+      title: 'Register',
+      caption: 'Photo by Katerina Radvanska - Unsplash',
+      message: ''
+    }
+    res.render('pages/register.ejs', meta)
   }
-
-  res.render('pages/register.ejs', meta)
 }
 
 function registerUser (req, res) {
@@ -138,7 +158,7 @@ function loginUser (req, res) {
     function onverify (match) {
       if (match) {
         req.session.user = {username: data.username}
-        res.redirect('/' + data.username)
+        res.redirect('/users/' + data.username)
       } else {
         var meta = {
           title: 'login',
@@ -153,7 +173,24 @@ function loginUser (req, res) {
 
 function feed (req, res) {
   if (req.session.user) {
-    res.send('Hoi hij doet het')
+    var meta = {
+      title: 'Feed'
+    }
+    
+    db.collection('users').findOne({
+      username: req.session.user.username
+    }, ifHaveData)
+    
+    function ifHaveData (err, dataUsers) {
+      if (err) throw err
+
+      db.collection('stories').find().toArray(done)
+
+      function done (err, dataStories) {
+        if (err) throw err
+        res.render('pages/feed', Object.assign({}, {dataUser: dataUsers, dataStories: dataStories }, meta))
+      }
+    }
   } else {
     loginFirst(res)
   }
@@ -179,10 +216,22 @@ function editProfile (req, res) {
 }
 
 function updateUser (req, res) {
-  console.log('De put request komt door.')
-  console.log(req.body)
+  var data = req.body
+  var user = req.session.user.username
 
-  res.redirect('/' + req.session.user.username)
+  db.collection('users').update(
+    {username: user},
+    { $set: {
+      name: data.name,
+      email: data.email,
+      place: data.place,
+      thumb: req.file.filename
+    }}, done)
+
+  function done (err) {
+    if (err) throw err
+    res.redirect('/users/' + user)
+  }
 }
 
 function logout (req, res) {
@@ -194,38 +243,143 @@ function logout (req, res) {
   }
 }
 
-function profile (req, res) {
-  var user = req.params.username
-
+function writeForm (req, res) {
   if (req.session.user) {
     db.collection('users').findOne({
-      username: user
+      username: req.session.user.username
     }, done)
   } else {
     loginFirst(res)
   }
 
   function done (err, data) {
+    if (err) throw err
+
     var meta = {
-      title: user,
-      session: req.session.user.username
+      title: 'Write a story'
     }
 
-    if (err) {
-      throw err
-    } else if(!data) {
-      res.status(404).render('pages/error.ejs', {title: 404, code: 404, message: 'Sorry, we can not help you with this request!'})
-    } else {
-      res.render('pages/profile.ejs', Object.assign({}, {data: data}, meta))
+    res.render('pages/write.ejs', Object.assign({}, {data: data}, meta))
+  }
+}
+
+function postStory (req, res) {
+  var data = req.body
+  var username = req.session.user.username
+
+  var today = new Date()
+  var dd = today.getDate()
+  var mm = today.getMonth()+1
+  var yyyy = today.getFullYear()
+
+  var date = dd +  '-' + mm + '-' + yyyy
+
+  db.collection('stories').insertOne({
+    date: date,
+    title: data.title,
+    where: data.where,
+    latitude: Number(data.lat),
+    longitude: Number(data.long),
+    banner: req.file.filename,
+    summary: data.summary,
+    story: data.story,
+    userData: {
+      username: username,
+      name: data.name,
+      thumb: data.thumb
     }
+  }, done)
+
+  function done (err, data) {
+    if (err) throw err
+    res.redirect('/feed')
+  }
+}
+
+function storyDetail (req, res) {
+  var id = req.params.id
+  var checkID
+
+  try {
+    checkID = new mongo.ObjectID(id)
+  } catch (err) {
+      res.status(400).render('pages/error.ejs', {
+      title: 400, 
+      code: 400, 
+      message: 'Sorry, we can not help you with this request!'
+    })
+    return
+  }
+
+  if (req.session.user) {
+    db.collection('stories').findOne({
+      _id: new mongo.ObjectID(id)
+    }, done)
+
+    function done (err, data) {
+      if (err) throw err
+      console.log(data)
+      console.log(id)
+      res.send('hij doet het')
+    }
+  } else {
+    loginFirst(res)
+  }
+}
+
+function profile (req, res) {
+  var user = req.params.username
+
+  if (req.session.user) {
+    db.collection('users').findOne({
+      username: user
+    }, ifHaveData)
+
+    function ifHaveData (err, dataUser) {
+      if (err) {
+        throw err
+      } else if(!dataUser) {
+        res.status(404).render('pages/error.ejs', {
+          title: 404, 
+          code: 404, 
+          message: 'Sorry, we can not help you with this request!'
+        })
+      } else { 
+        db.collection('stories').find({
+          'userData.username': user
+        }).toArray(done)
+
+        function done (err, dataStories) {
+          var meta = {
+            title: user,
+            session: req.session.user.username
+          }
+          res.render('pages/profile.ejs', Object.assign({}, {dataUser: dataUser, dataStories: dataStories}, meta))
+        }
+      }
+    }
+  } else {
+    loginFirst(res)
+  }
+}
+
+function notFound(req, res) {
+  if (req.session.user) {
+    res.status(404).render('pages/error.ejs', {
+      title: 404, 
+      code: 404, 
+      message: 'Sorry, we can not help you with this request!'
+    })
+  } else {
+    loginFirst(res)
   }
 }
 
 function loginFirst (res) {
   var meta = {
-      title: 'login',
-      caption: 'Photo by Katerina Radvanska - Unsplash',
-      message: 'You need to login first to make use of our service. Sorry! It`s a privacy thing!'
-    }
-    res.render('pages/login.ejs', meta)
+    title: 'login',
+    caption: 'Photo by Katerina Radvanska - Unsplash',
+    message: 'You need to login first to make use of our service. Sorry! It`s a privacy thing!'
+  }
+  res.render('pages/login.ejs', meta)
 }
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,12 @@
 /* Code from: https://stackoverflow.com/questions/13780309/get-filename-from-input-type-file-using-jquery?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa */
 
 $(document).ready(function (){
+	$('.storiewrapper').masonry({
+	  itemSelector: '.storypost',
+	  columnWidth: 355,
+	  gutter: 30
+	})
+
 	$('.fileInputLabel').click(function() {
 	  $('#fileInput').change(function() {
 	    var filename = $('#fileInput').val()
